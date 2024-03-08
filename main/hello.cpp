3a722a95d5810a03418a4712a4245ae22c6507ca
Xainab-U <139247678+Xainab-U@users.noreply.github.com>
2024-03-09 02:10:39 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run, Edited it to varify in Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "Editing it..\n";
   return 0;
 }
